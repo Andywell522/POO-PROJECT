f4588f912e5495fe4bacbe489a84714de7ff7cdc
Andywell522 <Andywell522@users.noreply.github.com>
2017-05-26 20:52:53 -0500

    Update interfaaz.java

--- src/interfaz/interfaaz.java
+++ src/interfaz/interfaaz.java
@@ -1,19 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package interfaz;
 
-/**
- *
- * @author Andres
- */
+
 public class interfaaz extends javax.swing.JFrame {
 
-    /**
-     * Creates new form interfaaz
-     */
+    
     public interfaaz() {
         initComponents();
     }
